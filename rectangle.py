5eff5ab987ff695d9ce116beba32b67c1362f3d0
arashid7868 <arashid6@student.cscc.edu>
2026-09-19 17:38:55 +0530

    Add rectangle calculation module

---
+++ rectangle.py
@@ -0,0 +1,18 @@
+"""
+Program Name: Geometry Calculator - Rectangle Module
+Author: Arashid7868
+Purpose: Provides functions for calculating the area and
+         perimeter of a rectangle.
+Starter Code: None; developed for Chapter 8 Lab 1.
+Date: September 19, 2026
+"""
+
+
+def calc_area(width, height):
+    """Calculate and return the area of a rectangle."""
+    return width * height
+
+
+def calc_perimeter(width, height):
+    """Calculate and return the perimeter of a rectangle."""
+    return 2 * (width + height)
